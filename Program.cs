47b009585214f75f18623e2905e5a15fb8d44cdf
M4C3RR <64778861+M4C3RR@users.noreply.github.com>
2020-08-06 09:52:42 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Text;
 using System.Linq;
 using System.Diagnostics;
@@ -9,92 +10,107 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
 
-
-namespace AsyncClient
+namespace AsyncListenerTcp
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
             Prompt();
-            Client.GetClientInfo();
+            Host.server.Start();
             Client.StartClient();
             Console.ReadLine();
         }
-
         static void Prompt()
         {
             Console.WriteLine(@"
-            ////////////////////////////////////////////////////////////`
-            ///                                                      ///`
-            ///   |||||||  ||      ||  ||||||| |\\   || ||||||||||   ///`
-            ///   ||       ||      ||  ||      ||\\  ||     ||       ///`
-            ///   ||       ||      ||  |||||   || \\ ||     ||       ///`
-            ///   ||       ||      ||  ||      ||  \\||     ||       ///`
-            ///   |||||||  ||||||  ||  ||||||| ||   \\|     ||       ///`
-            ///                                                      ///`
-            ////////////////////////////////////////////////////////////`
-            ````````````````````````````````````````````````````````````` "+"\n");
-            Console.WriteLine("Waiting for connection...\n");
-            Console.Write("Please enter a port number: ");
+            ///////////////////////////////////////////////////////////////////////`
+            ///                                                                 ///`
+            ///   ||     || ||||||| |||||||| ||||||| |\\   || ||||||| |||||||   ///`
+            ///   ||     || ||         ||    ||      ||\\  || ||      ||   ||   ///`
+            ///   ||     || |||||||    ||    |||||   || \\ || |||||   |||||||   ///`
+            ///   ||     ||      ||    ||    ||      ||  \\|| ||      ||  \\    ///`
+            ///   |||||| || |||||||    ||    ||||||| ||   \\| ||||||| ||   \\   ///`
+            ///                                                                 ///`
+            ///////////////////////////////////////////////////////////////////////`
+            ````````````````````````````````````````````````````````````````````````
+                                                                            " + " \n");
+            Console.Write("Please enter a port to listen to: ");
+            Host.GetHostInfo();
+            Console.WriteLine("\nListening on...");
+            Console.WriteLine($"\n{Host.host} {Host.ip} {Host.port}\n");
         }
     }
-    public class Client
+    public static class Host
     {
-        public static string host;
+        public static TcpListener server;
         public static IPAddress ip;
         public static int port;
-        public static TcpClient client;
-        public static Stream stream;
-        public static StreamReader reader;
-        public static StreamWriter writer;
-        public static string input;
+        public static string host;
 
-        public static void GetClientInfo()
+        public static void GetHostInfo()
         {
             host = Dns.GetHostName();
             IPHostEntry ipList = Dns.GetHostEntry(host);
             ip = ipList.AddressList[6];
             port = Int32.Parse(Console.ReadLine());
+            server = new TcpListener(ip, port);
         }
+        
+
+    }
+    public class Client
+    {
+        public static string input;
+        public static StreamReader reader;
+        public static StreamWriter writer;
+        public static Stream stream;
+        public static TcpClient client;
+
         public static async void StartClient()
         {
-            using (client = new TcpClient("10.0.0.125", port))
+            while (true)
             {
-                using (stream = client.GetStream())
+                using (client = Host.server.AcceptTcpClient())
                 {
-                    using (reader = new StreamReader(stream))
+                    using (stream = client.GetStream())
                     {
-                        using (writer = new StreamWriter(stream))
+                        using (reader = new StreamReader(stream))
                         {
-                            writer.AutoFlush = true;
-                            if (client.Connected)
-                                Console.WriteLine("\nConnected!\n");
-                            while (true)
+                            using (writer = new StreamWriter(stream))
                             {
-                                Task _ = ReceiveMessage();
+                                writer.AutoFlush = true;
 
-                                // Write
-                                Console.Write("=>  ");
-                                input = Console.ReadLine();
-                                await writer.WriteLineAsync(input);
+                                if (client.Connected)
+                                {
+                                    Console.WriteLine("Connected!\n"); 
+                                }
+                                while (true)
+                                {
+                                    Task _ = ReceiveMessage();
 
-                                // Read
-                                
+                                    // Send
+                                    Console.Write($"=>  ");
+                                    input = Console.ReadLine();
+                                    await writer.WriteLineAsync(input);
+
+                                    // Receive
+                                    
+                                }
                             }
                         }
                     }
                 }
-            }
+            }  
         }
         
         public static async Task ReceiveMessage()
         {
             while (true)
             {
+                
                 Console.Write($"{await reader.ReadLineAsync()}\n=>  ");
             }
         }
-        
     }
 }
